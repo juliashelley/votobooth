d232c9754aec94acca5c64bba334fda9ba0cc11a
Julia Shelley <j.c.shelley@btinternet.com>
2019-03-12 18:14:48 +0100

    update routes to be nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show, :update]
-  resources :elections
-  resources :candidatures
-  resources :votes, only: [:create]
-  resources :eligible_voters, only: [:index, :create, :destroy]
 
+  resource :dashboard, only: [:show]
+
+  resources :elections do
+    resources :eligible_voters, only: [:index, :create]
+    resources :candidatures, only: [:index, :new, :create]
+  end
+
+  resources :candidatures, only: [] do
+    resources :votes, only: [:create]
+  end
+
+  resources :eligible_voters, only: [:destroy]
+  resources :candidatures, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
