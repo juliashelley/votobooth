1c6a8309ebd88e2521d2e229514ce2c2aec856d7
Pontus Nordbrandt <pontus.nordbrandt@gmail.com>
2019-03-12 15:25:16 +0100

    add seeds for merging

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,25 +9,23 @@ User.create(
   first_name: 'Jimothy',
   last_name: 'James',
   email: 'jimjames@gmail.com',
-  password: 'password',)
+  password: 'password')
 
 puts 'Creating election seeds...'
 Election.create(
-  description: 'Acapella group president'
-  candidate_signup_deadline: Date.new(2019,3,07)
-  polls_close_at: Date.tomorrow
-  polls_open_at: Date.yesterday
-  results: 'Voting is still open'
+  name: 'Acapella group president',
+  status: 'Voting is still open!',
+  description: "Year twelve's chance to elect the new president of the acapella group",
+  voting_start_date: DateTime.yesterday,
+  voting_end_date: DateTime.tomorrow,
+  campaign_close_date: DateTime.tomorrow
   )
 
 puts 'Creating candidatures seeds...'
 Candidature.create(
-  first_name: User.first.first_name,
-  last_name: User.first.last_name,
-  user_id: User.first.id,
-  description: 'Toto lover'
-  campaign_promise: 'Fight for the right to sing africa in our acapella group'
-  photo:
-  total_votes: 0
+  name: User.first.first_name + " " + User.first.last_name,
+  status: 'Approved',
+  picture: 'url',
+  video: 'video_url'
   )
 puts "Done, let's migrate!"
