c2fec6da13e587ad2fe4c128019d3d0e323452e7
Pontus Nordbrandt <pontus.nordbrandt@gmail.com>
2019-03-12 15:08:01 +0100

    add user seed for election and candidature

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 puts 'Destroying seeds...'
 User.destroy_all
+Election.destroy_all
+Candidature.destroy_all
 
-puts 'Creating seeds'
-jim = User.create(
+
+puts 'Creating user seeds'
+User.create(
   first_name: 'Jimothy',
   last_name: 'James',
   email: 'jimjames@gmail.com',
   password: 'password',)
-puts 'Done!'
+
+puts 'Creating election seeds...'
+Election.create(
+  description: 'Acapella group president'
+  candidate_signup_deadline: Date.new(2019,3,07)
+  polls_close_at: Date.tomorrow
+  polls_open_at: Date.yesterday
+  results: 'Voting is still open'
+  )
+
+puts 'Creating candidatures seeds...'
+Candidature.create(
+  first_name: User.first.first_name,
+  last_name: User.first.last_name,
+  user_id: User.first.id,
+  description: 'Toto lover'
+  campaign_promise: 'Fight for the right to sing africa in our acapella group'
+  photo:
+  total_votes: 0
+  )
+puts "Done, let's migrate!"
