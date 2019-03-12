57c5e49f4654049883082328bacd5135b2f9a921
Julia Shelley <j.c.shelley@btinternet.com>
2019-03-12 17:34:44 +0100

    add user routes

--- app/controllers/elections_controller.rb
+++ app/controllers/elections_controller.rb
@@ -3,4 +3,26 @@ class ElectionsController < ApplicationController
   def index
     @elections = Election.where(user_id: current_user.id)
   end
+
+  def new
+    @election = Election.new
+  end
+
+  def create
+    @election = Election.new(election_params)
+    @election.user_id = current_user.id
+
+
+    if @election.save?
+      redirect_to elections_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def election_params
+    params.require(:election).permit!
+  end
 end
