381d39cedba663257d56a52fa68891efd12bfcf9
Pontus Nordbrandt <pontus.nordbrandt@gmail.com>
2019-03-12 14:42:17 +0100

    add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+User.new(
+  first_name: Jimothy,
+  last_name: James,
+  email: jimjames@gmail.com,
+  password: password)
