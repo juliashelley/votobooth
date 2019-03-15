9922a55a12d5c82ee224f95a5e66000706a419fa
Rebecca Green <rebeccalynngreen@gmail.com>
2019-03-15 11:04:14 +0100

    Add dotenv - Protect my secret data in .env file

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_120129) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.text "eligible_voters_string"
+    t.string "eligible_voters_string"
     t.index ["user_id"], name: "index_elections_on_user_id"
   end
 
