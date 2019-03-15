328b19911d39cf32ae50e5b9427c322166582283
Jordi Gramunt <jordi.gramunt@gmail.com>
2019-03-15 12:02:03 +0100

    fixed the delete on eligible voters

--- app/controllers/eligible_voters_controller.rb
+++ app/controllers/eligible_voters_controller.rb
@@ -4,4 +4,11 @@ class EligibleVotersController < ApplicationController
     @election = Election.find(params[:election_id])
   end
 
+  def destroy
+    @eligible_voter = EligibleVoter.find(params[:id])
+    @election = Election.find(@eligible_voter.election_id)
+    @eligible_voter.destroy
+    redirect_to new_election_eligible_voter_path(@election.id)
+  end
+
 end
