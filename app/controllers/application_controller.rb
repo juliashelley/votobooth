84ce794da2a52b48cd390998d78a08ca59b94e46
Pontus Nordbrandt <pontus.nordbrandt@gmail.com>
2019-03-19 10:12:34 +0100

    add pundit gem and policy file for election

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit
+
+   # Pundit: white-list approach.
+  after_action :verify_authorized, except: [ :home, :index ], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [ :home, :index ], unless: :skip_pundit?
+
+  #new election
+  #election show
+  #candidate show
+
+  # Uncomment when you *really understand* Pundit!
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # def user_not_authorized
+  #   flash[:alert] = "You are not authorized to perform this action."
+  #   redirect_to(root_path)
+  # end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
 end
---
+++ app/policies/election_policy.rb
@@ -0,0 +1,12 @@
+class ElectionPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+
+    def create?
+      return true
+    end
+
+  end
+end
