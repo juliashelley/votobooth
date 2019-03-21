3043a3c38470569a74644db2b61cb1ec8100b23b
Julia Shelley <j.c.shelley@btinternet.com>
2019-03-21 14:29:27 +0100

    add latest candidate seeds

--- app/models/election.rb
+++ app/models/election.rb
@@ -7,7 +7,8 @@ class Election < ApplicationRecord
   validates :voting_start_date, presence: true
 
   def set_time_remaining
-    day = voting_end_date.day - Time.now.day
+
+
     hour = voting_end_date.hour - Time.now.hour
     min = voting_end_date.min - Time.now.min
     sec = voting_end_date.sec - Time.now.sec
