9cca7266e7b368f8b25918789efca3f3a5452d18
Rebecca Green <rebeccalynngreen@gmail.com>
2019-03-21 12:12:00 +0100

    added methods to preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def eligible_email
+    user = User.first
+    UserMailer.eligible_email(user)
+  end
 
+  def existing_voter
+    user = User.first
+    UserMailer.existing_voter(user)
+  end
 end
